c5b645bdf339fa1000d3f71ea3fe7647f2b94cdc
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2026-02-15 11:59:47 +0100

    feat: init blockchain dev

---
+++ blockchain/contracts/IPadelRank.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.33;
+
+// @title IPadelRank
+// @notice Interface for the PadelRank contract, which manages player rankings.
+// @author MiKa Blondo
+interface IPadelRank {
+    
+}
---
+++ blockchain/contracts/PadelRank.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.33;
+
+import "./IPadelRank.sol";
+
+// @title PadelRank
+// @notice Implementation contract for managing player rankings in the PadelRank system.
+// @author MiKa Blondo
+contract PadelRank is IPadelRank {
+    
+}
